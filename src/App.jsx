4f19271007ebd6d9d3bca1d5e68d3ecd742e8a7e
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2025-04-02 18:12:53 -0500

    Crear components de Form y Options

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
+import Form from "./components/Form";
+import Options from "./components/Options";
+
 function App() {
   return (
     <>
       <h1 className="text-center text-5xl font-bold py-4 px-2">SnapShot</h1>
 
-      <form action="GET" className="w-full max-w-md mx-auto my-10 flex">
-        <input
-          type="search"
-          name="search"
-          id="search"
-          placeholder="Search..."
-          className="w-full bg-gray-200 px-4 py-2 rounded-tl-lg rounded-bl-lg"
-        />
-        <button className="w-20 bg-black flex justify-center items-center rounded-tr-lg rounded-br-lg">
-          <img src="/icons/search.svg" alt="search button" className="w-6" />
-        </button>
-      </form>
+      <Form />
+
+      <Options />
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+export default function Form() {
+  return (
+    <form action="GET" className="w-full max-w-md mx-auto my-10 flex">
+      <input
+        type="search"
+        name="search"
+        id="search"
+        placeholder="Search..."
+        className="w-full bg-gray-200 px-4 py-2 rounded-tl-lg rounded-bl-lg"
+      />
+      <button className="w-20 bg-black flex justify-center items-center rounded-tr-lg rounded-br-lg">
+        <img src="/icons/search.svg" alt="search button" className="w-6" />
+      </button>
+    </form>
+  );
+}
